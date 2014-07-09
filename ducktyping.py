41f1150f6dcd880f68c03d1f2e2e81e5491eb445
Alexander Schneider <wegi.pwnz@googlemail.com>
2014-07-09 12:35:45 +0200

    add ducktyping example

---
+++ ducktyping.py
@@ -0,0 +1,23 @@
+class Duck():
+    def make_sound(self):
+        print("Quack, Quack, schnatter, schnatter")
+
+    def __str__(self):
+        return "I'm a Frog"
+
+class Frog():
+    def make_sound(self):
+        print("Croooooak, Croooooak")
+
+    def __str__(self):
+        return "I'm a Penguin"
+
+class Human():
+    def articulate(self):
+        print("What is this zoo-nonsense going on?")
+
+    def __str__(self):
+        return "I'm a Human"
+
+
+############ Example 2   +
